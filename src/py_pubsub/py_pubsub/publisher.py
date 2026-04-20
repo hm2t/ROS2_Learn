dc0d253b313d1f8a76232356876bfc124e6a5a73
hmt <mingtaoh40@gmail.com>
2026-04-20 16:20:39 +0800

    提交一： 初始化仓库和.gitignore文件，添加py_pubsub包并添加两个节点(publisher和subscriber)

---
+++ src/py_pubsub/py_pubsub/publisher.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Publisher(Node):
+    def __init__(self):
+        super().__init__('publisher')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.timer = self.create_timer(0.5, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello, world! %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+def main(args=None):
+    rclpy.init(args=args)
+    publisher = Publisher()
+    rclpy.spin(publisher)
+    publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
